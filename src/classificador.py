9ae2502eb4f1c5180b760780bbe15d9b625c89ca
Samuel Vasconcelos <89222217+sammvasc@users.noreply.github.com>
2025-11-27 13:34:27 -0300

    Implement calcular_nivel function for ranking

---
+++ src/classificador.py
@@ -0,0 +1,27 @@
+def calcular_nivel(vitorias, derrotas):
+    saldo = vitorias - derrotas
+    
+    if vitorias < 10:
+        nivel = "Ferro"
+    elif 11 <= vitorias <= 20:
+        nivel = "Bronze"
+    elif 21 <= vitorias <= 50:
+        nivel = "Prata"
+    elif 51 <= vitorias <= 80:
+        nivel = "Ouro"
+    elif 81 <= vitorias <= 90:
+        nivel = "Diamante"
+    elif 91 <= vitorias <= 100:
+        nivel = "Lendário"
+    else:
+        nivel = "Imortal"
+    
+    return saldo, nivel
+
+
+vitorias = int(input("Digite o número de vitórias: "))
+derrotas = int(input("Digite o número de derrotas: "))
+
+saldo, nivel = calcular_nivel(vitorias, derrotas)
+
+print(f"O Herói tem de saldo de {saldo} está no nível de {nivel}.")
